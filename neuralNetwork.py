5226250d51dc8c0b41cc23e193d5cf0dc7a5936c
mk306207 <kolbermateusz@gmail.com>
2025-05-27 16:52:14 +0200

    venv

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -8,6 +8,7 @@ class neuralNetwork:
         except ValueError:
             print("Error - learning rate value should be a float datatype.")
             quit()
+        # bias and weights, 3 layers: 1-input, 2-hidden layer, 3-output
         
     def ReLU(v):
         return np.maximum(0,v)
