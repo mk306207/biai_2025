2453bd0c203151397ca3ea2906aeced7633972d1
mk306207 <kolbermateusz@gmail.com>
2025-05-28 23:18:57 +0200

    mocked a onehot data i should have recieved

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from neuralNetwork import neuralNetwork
 from imageOperations import imageSet
+from mockOneHot import meanColor, dominantColorLabel, onehot
+from PIL import Image
 from pathlib import Path
+import numpy as np
 
 p = Path('data/')
 
 
 i = imageSet(p)
-#i.print()
-imagesArray = i.asArray()
-print(imagesArray.shape)
-n = neuralNetwork()
-n.forwardPropagation(imagesArray)
+X = []
+Y = []
+for image in i.images:
+    mean_color = meanColor(image)
+    label = dominantColorLabel(mean_color)
+
+    X.append(mean_color)
+    Y.append(label)
+
+X = np.array(X)
+Y = np.array(Y)
+onehot_y = onehot(Y)
+
+# imagesArray = i.asArray()
+# print(imagesArray.shape)
+# n = neuralNetwork()
+# n.forwardPropagation(imagesArray)
 
---
+++ mockOneHot.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+from PIL import Image
+
+def meanColor(image):
+    img = image.copy().convert("RGB")
+    img = img.resize((64, 64))
+    pixels = np.array(img)
+    pixels = pixels.reshape(-1, 3)
+    mean_color = np.mean(pixels, axis=0) / 255  
+    mean_color = np.round(mean_color, 1)        
+    return np.array(mean_color)
+
+def dominantColorLabel(mean_color):
+    return int(np.argmax(mean_color))
+
+def onehot(y):
+    return np.eye(3)[y]
